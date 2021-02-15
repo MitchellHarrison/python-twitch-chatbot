2552e0b672f316e1f22bdc1edf93c744fa7558f5
MitchellHarrison <MitchInTheLab@gmail.com>
2021-02-15 16:31:56 -0500

    added command use tracking with .csv

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import socket
 import re
+import csv
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv("./credentials.env")
@@ -11,13 +13,24 @@ CHANNEL = "mitchsworkshop"
 SERVER = "irc.twitch.tv"
 PORT = 6667
 
+data_file_names = ["command_data.csv"]
+data_file_paths = ["./data/"+f for f in data_file_names]
+
+commands = [
+    "!discord", 
+    "!love", 
+    "!github", 
+    "!theme", 
+    "!specs"
+]
+
 class Bot():
-    def __init__(self):
-        self.server = SERVER
-        self.port = PORT
-        self.oauth_token = OAUTH_TOKEN
-        self.username = BOT_NAME
-        self.channel = CHANNEL
+    def __init__(self, server, port, token, username, channel):
+        self.server = server
+        self.port = port
+        self.oauth_token = token
+        self.username = username
+        self.channel = channel
 
 
     def connect_to_channel(self):
@@ -26,7 +39,7 @@ class Bot():
         self.irc_command(f"PASS oauth:{self.oauth_token}")
         self.irc_command(f"NICK {self.username}")
         self.irc_command(f"JOIN #{self.channel}")        
-        self.send_message(self.channel, "Happy Valentine's Day, everyone! <3")
+        self.send_message(self.channel, "I AM ALIVE!!")
         self.check_for_messages()
 
     
@@ -64,41 +77,70 @@ class Bot():
             pass
 
 
+    def write_command_data(self, user, command):
+        entry = {
+            "time": str(datetime.now()),
+            "user": user,
+            "command": command
+        }
+        field_names = entry.keys()
+        with open("./data/command_data.csv", "a") as file:
+            writer = csv.DictWriter(file, fieldnames = field_names)
+            writer.writerow(entry)
+
+
     # define and execute each command
     def execute_command(self, user, command):
-        if command == "!love":
-            self.send_message(
-                self.channel, 
-                f"I love you, @{user}"
-            )
+        if command in commands:
+            if command == "!love":
+                self.send_message(
+                    self.channel, 
+                    f"I love you, @{user}"
+                )
 
-        if command == "!github":
-            self.send_message(
-                self.channel, 
-                "See past on-stream projects on Mitch's GitHub here! https://github.com/MitchellHarrison"
-            )
+            if command == "!github":
+                self.send_message(
+                    self.channel, 
+                    "See past on-stream projects on Mitch's GitHub here! https://github.com/MitchellHarrison"
+                )
 
-        if command == "!discord":
-            self.send_message(
-                self.channel, 
-                "Give or receive help or engage in nerdy debauchery in The Workshop discord server! https://discord.gg/9yFFNpP"
-            )
+            if command == "!discord":
+                self.send_message(
+                    self.channel, 
+                    "Give or receive help or engage in nerdy debauchery in The Workshop discord server! https://discord.gg/9yFFNpP"
+                )
 
-        if command == "!theme":
+            if command == "!theme":
+                self.send_message(
+                    self.channel,
+                    "Current VSCode theme is Monokai Vibrant!"
+                )
+
+            if command == "!specs":
+                self.send_message(
+                    self.channel,
+                    "CPU - i7 9700k; GPU - RTX 2080; RAM - 16GB Trident Z DDR4"
+                )
+
+            self.write_command_data(user, command)
+        else:
             self.send_message(
                 self.channel,
-                "Current VSCode theme is Monokai Vibrant!"
+                f"That's not a valid command, @{user}"
             )
 
-        if command == "!specs":
-            self.send_message(
-                self.channel,
-                "CPU - i7 9700k; GPU - RTX 2080; RAM - 16GB Trident Z DDR4"
-            )
+
+def check_for_data_files(files: list, fieldnames: list):
+    for f in files:
+        if f not in os.listdir("./data"):
+            with open(f, "w") as file:
+                writer = csv.DictWriter(file, fieldnames = fieldnames)
+                writer.writeheader()
 
 
 def main():
-    bot = Bot()
+    check_for_data_files(data_file_names, ["time", "user", "command"])
+    bot = Bot(SERVER, PORT, OAUTH_TOKEN, BOT_NAME, CHANNEL)
     bot.connect_to_channel()
 
 

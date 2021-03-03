ce6bead39467bc75cc4f661ac72fd53d059fc5b3
MitchellHarrison <MitchInTheLab@gmail.com>
2021-03-02 19:51:26 -0500

    improve type hinting, implement text commands

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
-import socket
 import re
+import socket
 import sqlite3
 import command
 from datetime import datetime
 
 class Bot():
-    def __init__(self, server, port, token, username, channel):
+    def __init__(self, server: str, port: int, token: str, username: str, channel: str, text_commands: dict):
         self.server = server
         self.port = port
         self.oauth_token = token
         self.username = username
         self.channel = channel
         self.commands = {s.command_name: s for s in (c(self) for c in command.CommandBase.__subclasses__())}
+        self.text_commands = text_commands
 
 
     # connect to IRC server and begin checking for messages
@@ -47,15 +48,22 @@ class Bot():
     def parse_message(self, message: str):
         try:
             # regex pattern
-            pat_message = re.compile(r":(?P<user>.+)!.+#mitchsworkshop :(?P<text>.+)", flags=re.IGNORECASE)
+            pat_message = re.compile(fr":(?P<user>.+)!.+#{self.channel} :(?P<text>.+)", flags=re.IGNORECASE)
+            
             # pull user and text from each message
             user = re.search(pat_message, message).group("user")
             text = re.search(pat_message, message).group("text")
 
+            # respond to server pings
+            if text.lower().startswith("ping"):
+                print("ping received")
+                self.irc_command("PONG")
+                print("pong sent")
+
             # check for commands being used
             if text.startswith("!"):
                 command = text.split()[0]
-                self.execute_command(user, command)
+                self.execute_command(user, command, text)
             self.store_message_data(user)
 
         except AttributeError:
@@ -71,7 +79,7 @@ class Bot():
             "user" : user
         }
         with conn:
-            cursor.execute("INSERT INTO chat_messages VALUES (:time, :user)", entry)
+            cursor.execute("INSERT INTO chat_messages (time, user) VALUES (:time, :user)", entry)
         cursor.close()
         conn.close()
 
@@ -86,12 +94,32 @@ class Bot():
             "command" : command
         }
         with conn:
-            cursor.execute("INSERT INTO command_use VALUES (:time, :user, :command)", entry)
+            cursor.execute("INSERT INTO command_use (time, user, command) VALUES (:time, :user, :command)", entry)
         cursor.close()
         conn.close()
 
-        # execute each command
-    def execute_command(self, user: str, command: str):
+
+    # execute each command
+    def execute_command(self, user: str, command: str, message: str):
         if command in self.commands.keys():
-            self.commands[command].execute(user)   
+            self.commands[command].execute(user, message)   
             self.store_command_data(user, command)
+
+        elif command in self.text_commands.keys():
+            self.send_message(
+                self.channel,
+                self.text_commands[command]
+            )
+            self.store_command_data(user, command)
+        
+        self.text_commands = self.reload_text_commands()
+
+
+    def reload_text_commands(self):
+        conn = sqlite3.connect("data.db")
+        cursor = conn.cursor()
+        with conn:
+            commands = {k:v for k,v in [e for e in cursor.execute("SELECT * FROM text_commands")]}
+        cursor.close()
+        conn.close()
+        return commands

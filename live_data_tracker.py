0c51c24944ecdbf5b4d49601dc6d53f87f6dd03d
MitchellHarrison <MitchInTheLab@gmail.com>
2021-03-10 13:02:09 -0500

    get stream data once per minute

--- live_data_tracker.py
+++ live_data_tracker.py
@@ -3,6 +3,7 @@ import json
 import requests 
 import os  
 import time
+import sched
 from dotenv import load_dotenv
 from datetime import datetime
 
@@ -11,6 +12,18 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 OAUTH_TOKEN = os.getenv("OAUTH_TOKEN")
 CHANNEL = os.getenv("CHANNEL")
+
+conn = sqlite3.connect("data.db")
+cursor = conn.cursor()
+with conn:
+    cursor.execute("""CREATE TABLE IF NOT EXISTS live_data 
+                        (time text, 
+                        user text, 
+                        language text,
+                        game_name text,
+                        viewer_count integer,
+                        title text,
+                        tags text);""")   
 
 def get_bearer(client_id: str, client_secret: str) -> str:
     url = f"https://id.twitch.tv/oauth2/token"
@@ -25,30 +38,70 @@ def get_bearer(client_id: str, client_secret: str) -> str:
     return bearer
 
 
-def get_live_status(channel: str, client_id: str, bearer: str) -> bool:
-    url = f"https://api.twitch.tv/helix/search/channels?query={channel}" 
+def get_stream_data(channel: str, client_id: str, bearer: str) -> dict:
+    url = f"https://api.twitch.tv/helix/streams/?user_login={channel}"
     headers = {
         "client-id" : client_id,
-        "authorization" : f"Bearer {bearer}"
+        "authorization": f"Bearer {bearer}"
     }
     response = requests.get(url, headers = headers)
     data = json.loads(response.content)
-    is_live = data["data"][0]["is_live"]
-    return is_live
+    return data["data"][0]
+   
 
-
-def get_channel_id(channel: str, client_id: str, bearer: str) -> str:
-    url = f"https://api.twitch.tv/helix/search/channels?query={channel}" 
+def get_tags(channel: str, client_id: str, bearer: str, broadcaster_id: str) -> list:
+    url = f"https://api.twitch.tv/helix/streams/tags?broadcaster_id={broadcaster_id}"
     headers = {
-        "client-id" : client_id,
-        "authorization" : f"Bearer {bearer}"
+        "client-id": client_id,
+        "authorization": f"Bearer {bearer}"
     }
-    response = requests.get(url, headers = headers)
-    data = json.loads(response.content)
-    channel_id = data["data"][0]["id"]
-    return channel_id
+    response = requests.get(url, headers=headers)
+    tags_data = json.loads(response.content)["data"]
+    tags = []
+    for t in tags_data:
+        tags.append(t["localization_names"]["en-us"])
+    return tags
+
+
+def write_live_data(entry):
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
+    with conn:
+        cursor.execute(f"""INSERT INTO live_data 
+                        (time, user, language,game_name, viewer_count, title, tags) 
+                        VALUES 
+                        (:time, :user, :language, :game_name, :viewer_count, :title, :tags)""", entry)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
 
 bearer = get_bearer(CLIENT_ID, CLIENT_SECRET)
-is_live = get_live_status(CHANNEL, CLIENT_ID, bearer)
-channel_id = get_channel_id(CHANNEL, CLIENT_ID, bearer)
+stream_data = get_stream_data(CHANNEL, CLIENT_ID, bearer) 
+is_live = stream_data["type"] == "live"
+broadcaster_id = stream_data["user_id"]
+current_min = datetime.now().minute
+print(current_min)
+
+while is_live:
+    if current_min != datetime.now().minute:
+        current_min = datetime.now().minute
+        username = stream_data["user_name"]
+        language = stream_data["language"]
+        game_name = stream_data["game_name"]
+        viewer_count = stream_data["viewer_count"]
+        title = stream_data["title"]
+        tags_list = get_tags(CHANNEL, CLIENT_ID, bearer, broadcaster_id)
+
+        live_entry = {
+            "time" : datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "user" : username,
+            "language" : language,
+            "game_name" : game_name,
+            "viewer_count" : viewer_count,
+            "title" : title,
+            "tags" : str(tags_list)
+        }
+        write_live_data(live_entry)
+        print("entry made!")

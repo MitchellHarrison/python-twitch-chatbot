c88ed13a0886558e1798b637a445240481ae43dd
MitchellHarrison <MitchInTheLab@gmail.com>
2021-03-25 22:56:08 -0400

    add !followage (!uptime currently broken)

--- command.py
+++ command.py
@@ -2,6 +2,7 @@ import requests
 import random
 import json
 import sqlite3
+from datetime import datetime, timedelta
 from abc import ABC, abstractmethod
 
 class CommandBase(ABC):
@@ -69,7 +70,15 @@ class DeleteCommand(CommandBase):
 
     def execute(self, user, message):
         if user == self.bot.channel:
-            first_word = message.split()[1]
+            try:
+                first_word = message.split()[1]
+            except IndexError:
+                self.bot.send_message(
+                    self.bot.channel,
+                    "You didn't select a command to delete!"
+                )
+                return
+
             command = first_word if first_word.startswith("!") else "!" + first_word
 
             conn = sqlite3.connect("data.db")
@@ -209,3 +218,112 @@ class CommandsCommand(CommandBase):
             message = commands_str
         )
 
+
+class FollowAgeCommand(CommandBase):
+    @property 
+    def command_name(self):
+        return "!followage"
+
+    
+    def execute(self, user, message):
+        conn = sqlite3.connect("data.db")
+        cursor = conn.cursor()
+
+        if len(message.split()) > 1:
+            user = message.split()[1]
+
+        with conn:
+            cursor.execute(f"""SELECT time FROM followers
+                                WHERE username='{user.lower()}'""")
+            try:
+                follow_time_str = cursor.fetchone()[0]
+            except TypeError:
+                self.bot.send_message(
+                    channel = self.bot.channel,
+                    message = f"{user} is not currently following {self.bot.channel} or is a new follower."
+                )
+                return
+        cursor.close()
+        conn.close()
+        
+        follow_time = datetime.strptime(follow_time_str, "%Y-%m-%dT%H:%M:%SZ")
+        delta = datetime.now() - follow_time
+
+        years = delta.days // 365
+        days = delta.days - (years*365)
+        hours = delta.seconds // 3600
+        minutes = (delta.seconds - hours*3600) // 60
+        seconds = delta.seconds - (hours * 3600) - (minutes * 60)
+        follow_stats = {
+            "year" : years,
+            "day" : days,
+            "hour" : hours,
+            "minute" : minutes,
+            "second" : seconds
+        }
+
+        message = f"@{user} has been following for"
+        for k,v in follow_stats.items():
+            if v > 0:
+                message += f" {v} {k}"
+                if v > 1:
+                    message += "s"
+        message += "!"
+        self.bot.send_message(
+            channel = self.bot.channel,
+            message = message
+        )
+
+
+# class UptimeCommand(CommandBase):
+#     @property
+#     def command_name(self):
+#         return "!uptime"
+
+    
+#     def execute(self, user, message):
+#         url = f"https://api.twitch.tv/helix/streams?user_id={self.bot.user_id}"
+#         headers = {
+#             "client-id": self.bot.client_id,
+#             "authorization" : f"Bearer {self.bot.bearer}"
+#         }
+#         response = requests.get(url, headers = headers, timeout=3)
+#         data = json.loads(response.content)
+#         start_time = data["data"][0]["started_at"]
+#         # try:
+#         #     start_time = data["data"][0]["started_at"]
+#         # except KeyError:
+#         #     self.bot.send_message(
+#         #         channel = self.bot.channel,
+#         #         message = f"{self.bot.channel} is not currently live! :("
+#         #     )
+#         #     return     
+
+#         delta = datetime.now() - start_time
+
+#         years = delta.days // 365
+#         days = delta.days - (years*365)
+#         hours = delta.seconds // 3600
+#         minutes = (delta.seconds - hours*3600) // 60
+#         seconds = delta.seconds - (hours * 3600) - (minutes * 60)
+
+#         follow_stats = {
+#             "year" : years,
+#             "day" : days,
+#             "hour" : hours,
+#             "minute" : minutes,
+#             "second" : seconds
+#         }
+
+#         message = f"@{user}, {self.bot.channel} has been live for"
+#         for k,v in follow_stats.items():
+#             if v > 0:
+#                 message += f" {v} {k}"
+#                 if v > 1:
+#                     message += "s"
+#         message += "!"
+
+#         self.bot.send_message(
+#             channel = self.bot.channel,
+#             message = message
+#         )

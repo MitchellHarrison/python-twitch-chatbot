e3f2bd9f61122ae433d052952d0dea1fad742f09
Mitchell Harrison <mitchha22ison@gmail.com>
2021-05-24 14:30:42 -0400

    implemented front end for retrieving bearer token from database

--- environment.py
+++ environment.py
@@ -1,23 +1,40 @@
+from database import Base, Session, engine
+from sqlalchemy import select, delete, insert
+from models import Tokens
 import requests 
 import os 
 import json
 from dotenv import load_dotenv
 load_dotenv("./credentials.env")
 
+Base.metadata.create_all(bind=engine)
+session = Session()
+
 class Environment():
     def __init__(self):
+        # get creds from env file
         self.channel = os.getenv("CHANNEL")
         self.bot_name = os.getenv("BOT_NAME")
         self.client_id = os.getenv("CLIENT_ID")
         self.client_secret = os.getenv("CLIENT_SECRET")
         self.oauth = os.getenv("OAUTH_TOKEN")
+
+        # these are pre-defined
         self.irc_port = 6667
         self.irc_server = "irc.twitch.tv"
-        self.bearer = self.get_bearer()
+
         self.user_id = self.get_user_id()
 
     
-    def get_bearer(self) -> str:
+    # get new bearer token
+    def refresh_bearer(self) -> None:
+        # delete existing bearer
+        engine.execute(
+            delete(Tokens)
+            .where(Tokens.name == "Bearer")
+        )
+
+        # get new bearer from Twitch
         url = "https://id.twitch.tv/oauth2/token"
         params = {
             "client_id" : self.client_id,
@@ -27,14 +44,34 @@ class Environment():
         response = requests.post(url, params = params, timeout=3)
         data = json.loads(response.content)
         bearer = data["access_token"]
-        return bearer
 
+        # write new bearer to database
+        entry = {
+            "name": "Bearer",
+            "token": bearer
+        }
+        engine.execute(
+            insert(Tokens)
+            .values(entry)
+        )
+
+    
+    # get bearer from database
+    def get_bearer(self) -> str:
+        result = engine.execute(
+            select(Tokens.token)
+            .where(Tokens.name == "Bearer")
+        ).fetchone()
+
+        # return bearer from result
+        bearer = result[0]
+        return bearer
 
     def get_user_id(self) -> str:
         url = f"https://api.twitch.tv/helix/users?login={self.channel}"
         headers = {
             "client-id": self.client_id,
-            "authorization": f"Bearer {self.bearer}"
+            "authorization": f"Bearer {self.get_bearer()}"
             }
         response = requests.get(url, headers = headers)
         data = json.loads(response.content)
@@ -42,3 +79,5 @@ class Environment():
         return user_id
 
 env = Environment()
+env.get_bearer()
+
